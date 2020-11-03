c0b652e1871218f6116442910750795e5c6fa583
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-11-03 16:08:06 +0700

    commit init dự án

---
+++ src/main/java/com/example/fourkingmusic/FourkingmusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.fourkingmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourkingmusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourkingmusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fourkingmusic/FourkingmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fourkingmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourkingmusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
