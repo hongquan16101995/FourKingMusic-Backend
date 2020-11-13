865b4ac960851853be6b55140088104e29ff5100
Mon0106 <dongnam.japan@gmail.com>
2020-11-13 08:46:35 +0700

    lỗi lòi mắt

--- src/main/java/com/example/fourkingmusic/repository/CommentSongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.fourkingmusic.repository;
-
-import com.example.fourkingmusic.models.CommentSong;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentSongRepository extends CrudRepository<CommentSong, Long> {
-}
